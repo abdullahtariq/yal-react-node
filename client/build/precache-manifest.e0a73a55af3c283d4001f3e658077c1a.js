f3f885b908b86c698b8cac8b1275d87bfea8827d
abdullahtariq <faabdullahtariq@gmail.com>
2020-03-01 01:51:01 +0500

    init commit

---
+++ client/build/precache-manifest.e0a73a55af3c283d4001f3e658077c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c376cca49fa8301c280729d19f015370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19e394ad9eef334daaed",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "55fc035acbb1ed02d90e",
+    "url": "/static/js/2.fdde5720.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.fdde5720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e394ad9eef334daaed",
+    "url": "/static/js/main.5cce0911.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
